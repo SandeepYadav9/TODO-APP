3f52e69d7fcce13a69ba715d5553b214f4b63af2
SandeepCbnits <sandeep.yadav@cbnits.com>
2022-08-05 17:24:16 +0530

    SUBMITED-INPUT-TO-DO

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,8 +2,18 @@ import React, { useState } from "react";
 import style from "./Input.module.css";
 const Input = () => {
   const [input, setInput] = useState("");
-  const [textInput, setTextInput] = useState([]);
-
+  const [listData, setListData] = useState([]);
+  const submitHandler = (e) => {
+    console.log("value", e);
+    if (e.code === "Enter") {
+      const listArr = [...listData];
+      listArr.push(input);
+      setListData(listArr);
+      setInput("");
+    }
+    // setListData(e.target.value)
+    // setListData(input);
+  };
   return (
     <div className={style.newItem}>
       <input
@@ -12,10 +22,12 @@ const Input = () => {
         onChange={(e) => {
           setInput(e.target.value);
         }}
+        onKeyDown={submitHandler}
         placeholder="What need to be done ?"
       />
+
       <ul>
-        {textInput.map((list) => {
+        {listData.map((list) => {
           return <li>{list}</li>;
         })}
       </ul>
