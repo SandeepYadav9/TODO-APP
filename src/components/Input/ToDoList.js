0c5ddd5801f1f236dcb51b3b1a49821274700068
SandeepCbnits <sandeep.yadav@cbnits.com>
2022-08-07 23:11:09 +0530

    UPDATED-LIST

--- src/components/Input/ToDoList.js
+++ src/components/Input/ToDoList.js
@@ -2,15 +2,16 @@ import React from "react";
 
 import style from "./ToDoList.module.css";
 const ToDoList = (props) => {
+ 
   return (
-    <ul className={style.listToDo}>
+    <ul className={style.todolist}>
       {props.listData.map((list) => {
         return (
           <li>
             <div className={style.view}>
               <input className={style.toggle} type="checkbox" />
               <label>{list}</label>
-              <button className={style.delete}>Delete</button>
+              <button className={style.delete} >Delete</button>
             </div>
           </li>
         );
