f7d49329d2f7506f6d3e41971ff2c7f4b0ad1051
SandeepCbnits <sandeep.yadav@cbnits.com>
2022-08-10 01:58:12 +0530

    TO-DO-LIST

--- src/TodoMvc.js
+++ src/TodoMvc.js
@@ -1,101 +1,19 @@
-import React, { useState } from "react";
-import Input from "./components/Input/Input";
+import React from "react";
+
 import Title from "./components/Title/Title";
 import style from "./TodoMvc.module.css";
 import Card from "./components/UI/Card";
-import Footer from "./components/Footer/Footer";
+
 import ToDoList from "./components/Input/ToDoList";
 
 const TodoMvc = () => {
-  const [count, setCount] = useState(0);
-  const [toggle, setToggle] = useState(false);
-  const [input, setInput] = useState("");
-  const [listData, setListData] = useState([]);
-
-  // Updated List Handler
-  const addListHandler = (e) => {
-    if (e) {
-      const listArr = [...listData];
-      listArr.push({
-        id: Date.now(),
-        title: e,
-        status: "active",
-      });
-      setListData(listArr);
-      setToggle(true);
-      setCount(count + 1);
-    }
-  };
-
-  // Distroy Task
-  const onDeleteHandler = (id) => {
-    const lestArr = [...listData];
-    lestArr.splice(id, 1);
-    setListData(lestArr);
-    setCount(count - 1);
-  };
-  // Filter, Completed
-
-  const taskIsCompledHandler = (id) => {
-   console.log(id)
-  };
-
-  // isActive Task
-  const isActiveHandler = (id) => {
-    // let compledActiveList = [...listData];
-    // console.log(compledActiveList);
-    // let updatedListIndex = compledActiveList.findIndex(
-    //   (list) => list.id !== id
-    // );
-    // compledActiveList[updatedListIndex].status = "active";
-    // setListData(updatedListIndex);
-    console.log(id)
-  };
-
-  // All Task
-
-  const allListHandler = () => {
-    const listArr = [...listData];
-    listArr.push(input);
-    setToggle(false);
-    return listArr;
-  };
-  const clearListHandler = (id) => {
-    // All clear List
-  };
-
-  const clearList = (id) => {
-    //Clear List
-  };
-  const onCompletHandler = (list) => {
-    let compledList = [...listData];
-    let index = compledList.findIndex((list, index) => list.list === list);
-    compledList[index].status = true;
-    console.log(compledList);
-    console.log(compledList);
-  };
   return (
     <Card>
       <Title />
       <div className={style.container}>
-        <Input submitHandler={addListHandler} clearHandler={clearList} />
+        <ToDoList />
 
-        {toggle && (
-          <ToDoList
-            onCompleted={() => onCompletHandler()}
-            listData={listData}
-            onDelet={onDeleteHandler}
-          />
-        )}
-        {toggle && (
-          <Footer
-            countText={count}
-            allList={allListHandler}
-            taskIsCompled={taskIsCompledHandler}
-            isActive={isActiveHandler}
-            clearList={clearListHandler}
-          />
-        )}
+        
       </div>
     </Card>
   );
--- src/components/Input/ToDoList.js
+++ src/components/Input/ToDoList.js
@@ -1,39 +1,126 @@
 import React, { useState } from "react";
-
 import style from "./ToDoList.module.css";
-const ToDoList = (props) => {
-  const [isCompleted, setIsCompleted] = useState(false);
 
-  const onClickCheckHandler = (list) => {
-    setIsCompleted(true);
-    props.onCompleted(list);
+
+const ToDoList = (props) => {
+  const [listData, setListData] = useState([]);
+  const [value, setValue] = useState("");
+  const [count, setCount] = useState(0);
+  const [masterData, setMasterData] = useState([]);
+
+  const addListHandler = (e) => {
+    if (value && e.keyCode === 13) {
+      const listArr = [...listData];
+      listArr.push({
+        id: Date.now(),
+        title: value,
+        status: "active", // active/completed
+        selected: "inactive", // inactive/active
+        isChecked: false, // true/false
+      });
+      setValue("");
+      setListData(listArr);
+      setMasterData(listArr);
+      setCount(count + 1);
+    }
   };
-  let completedTask = `{ ${isCompleted} ?  ${!style.completed}  : ${
-    style.completed
-  }}`;
+  const onCompletHandler = (id) => {
+    const listData1 = listData.map((list) => {
+      if (list.id === id) {
+        if (!list.isChecked) {
+          list.status = "completed";
+          list.selected = "active";
+          list.isChecked = true;
+        } else {
+          list.status = "active";
+          list.selected = "inactive";
+          list.isChecked = false;
+        }
+      }
+      return list;
+    });
+
+    setListData(listData1);
+    setMasterData(listData1);
+  };
+  const onDeleteHandler = (id) => {
+    const listData1 = listData.filter((list) => list.id !== id);
+    setCount(count - 1);
+    setListData(listData1);
+    setMasterData(listData1);
+  };
+
+  const filterTask = (status) => {
+    if (status === "all") {
+      setListData(masterData);
+    } else {
+      const listData1 = masterData.filter((list) => list.status === status);
+      setListData(listData1);
+    }
+    setCount(listData.length);
+  };
+
+  const clearCompletedList = () => {
+    const listData1 = masterData.filter((list) => list.status !== "completed");
+    setListData(listData1);
+    setMasterData(listData1);
+  };
+
   return (
-    <ul className={style.todolist}>
-      {props.listData.map((list, index) => {
-        return (
-          <li
-            key={list.id}
-            className={completedTask}
-            onClick={() => onClickCheckHandler(list.id)}
-          >
-            <div className={style.view}>
-              <input className={style.toggle} type="checkbox" />
-              <label>{list.title}</label>
-              <button
-                className={style.delete}
-                onClick={() => props.onDelet(index)}
-              >
-                X
-              </button>
-            </div>
+    <div>
+      <div className={style.newItem}>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onKeyDown={(e) => addListHandler(e)}
+          placeholder="What need to be done ?"
+          onClick={props.clearHandler}
+        />
+      </div>
+      <ul className={style.todolist}>
+        {listData.map((list, index) => {
+          return (
+            <li key={list.id} className={style.completed}>
+              <div className={style.view}>
+                <input
+                  className={style.toggle}
+                  onChange={() => onCompletHandler(list.id)}
+                  type="checkbox"
+                  checked={list.isChecked}
+                />
+                <label className={style[list.selected]}>{list.title}</label>
+                <button
+                  className={style.delete}
+                  onClick={() => onDeleteHandler(list.id)}
+                >
+                  X
+                </button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+      <div className={style.footer}>
+        <div className={style.todoCount}>
+          <span>
+            <strong>{count}</strong> items left
+          </span>
+        </div>
+        <ul className={style.listItem}>
+          <li onClick={() => filterTask("all")}>
+            <a href="#">All</a>
           </li>
-        );
-      })}
-    </ul>
+          <li onClick={() => filterTask("active")}>
+            <a href="#"> Active </a>
+          </li>
+          <li onClick={() => filterTask("completed")}>
+            <a href="#">Completed</a>
+          </li>
+        </ul>
+        <button onClick={() => clearCompletedList()}>Clear completed</button>
+      </div>
+    </div>
   );
 };
 
